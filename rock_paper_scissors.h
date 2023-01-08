600d169561e1d1ced053f115ebe6e8dccedcf2dc
lspohle <lea.spoehle@gmail.com>
2023-01-08 05:03:26 +0100

    Update

--- rock_paper_scissors.h
+++ rock_paper_scissors.h
@@ -1,11 +1,7 @@
 #ifndef ROCK_PAPER_SCISSORS_H
 # define ROCK_PAPER_SCISSORS_H
 # include "libft/libft.h"
-# include <stdio.h>
-# include <stdlib.h>
 # include <time.h>
-#include <unistd.h>
-#include <string.h>
 # define RED "\e[38;5;196;1m"
 # define PURPLE "\e[38;5;141;1m"
 # define YELLOW "\e[38;5;226;1m"
